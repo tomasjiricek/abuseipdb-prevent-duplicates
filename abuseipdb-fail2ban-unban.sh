16779ac1dd8334dfa2b9d9a89732f0f85b1b089d
Tomas Jiricek <tjiricek@email.cz>
2020-03-26 20:31:55 +0100

    add ban length, time of ban (#1)
    * refactor script for reporting to save ban length and time of banning
    
    * change the script to use fail2ban SQLite DB for time of ban
    
    * re-report when banning already banned IP with different ban length
    
    * remove unban script

--- abuseipdb-fail2ban-unban.sh
+++
@@ -1,9 +0,0 @@
-REPORTED_IP_LIST_FILE=~/abuseipdb-reported-ip-list
-
-APIKEY=$1
-IP=$2
-
-sed -i "/^$IP\$/d" $REPORTED_IP_LIST_FILE
-
-# TODO: Make an unban request to AbuseIPDB if it's possible
-# TODO: Add script configuration to actionunban if the first TODO is applicable
